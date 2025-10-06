ee1c22046e60957e5a55b7919de23bfa1d1cd78c
Rajput-Pallavi <rajputpallavi737@gmail.com>
2025-10-06 22:14:25 +0530

    main page edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainApp from './Pages/MainApp/MainApp';
 import SignIn from'../src/Pages/SingnIn/SignIn';
+import Shorts from './Pages/Shorts/Shorts';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Default page */}
+       
         <Route path="/" element={<MainApp />} />
 
-        {/* Sign In page */}
+        
         <Route path="/signin" element={<SignIn />} />
+
+          <Route path="/Shorts" element={<Shorts/>}> </Route>
       </Routes>
     </Router>
   )
