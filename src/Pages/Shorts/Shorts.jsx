426e369bd595ea805a242e7a3077d9d230273bca
Rajput-Pallavi <rajputpallavi737@gmail.com>
2025-10-12 16:50:45 +0530

    frontend main

--- src/Pages/Shorts/Shorts.jsx
+++ src/Pages/Shorts/Shorts.jsx
@@ -9,20 +9,7 @@ const Shorts = (setSearchValue, searchValue, handleSearch) => {
       <div className='log'>
      <img src={logo} alt="" />
       </div>
-      <div className="search-bar">
-      <input
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
-        placeholder="Search..."
-        onKeyDown={(e) => {
-          if (e.key === "Enter") handleSearch();
-        }}
-      />
-      <button className="search-arrow" onClick={handleSearch} tabIndex={-1}>
-        &gt;
-      </button>
-    
-    </div>
+     
  </div>
   )
 }
